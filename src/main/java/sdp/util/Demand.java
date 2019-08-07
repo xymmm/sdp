6b11cf9f1e52cada92e779220de111e17fc7b7af
gwr3n <robros@gmail.com>
2019-08-07 16:23:37 +0100

    Updated Demand

--- src/main/java/sdp/util/Demand.java
+++ src/main/java/sdp/util/Demand.java
@@ -12,24 +12,9 @@ public class Demand {
       int maxDemand = dist.inverseCumulativeProbability(1-tail);
       double[] demandProbabilities = new double[maxDemand + 1];
       
-      //double normalizationFactor = dist.inverseCumulativeProbability(tail) - dist.probability(0);
-      
       for(int i=0;i<=maxDemand;i++) {
          demandProbabilities [i] = dist.probability(i)/dist.cumulativeProbability(maxDemand);
       }
       return demandProbabilities;
-   }
-   
-   public static void main(String args[]) {
-	   
-	   double[] demandProbabilities = new double[250 + 1];
-	   double tail = 0.9;
-	   
-	   PoissonDistribution dist = new PoissonDistribution(50);
-	   
-	   int prob = dist.inverseCumulativeProbability(0.9);
-	   System.out.println(prob);
-	   
-	   
    }
 } 
