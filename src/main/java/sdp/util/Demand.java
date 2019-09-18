12fd6085419f4553c4b089926e898fba22d62beb
xymmm <xyma26@163.com>
2019-09-18 12:25:37 +0100

    generate normal distribution

--- src/main/java/sdp/util/Demand.java
+++ src/main/java/sdp/util/Demand.java
@@ -1,5 +1,6 @@
 package sdp.util;
 
+import org.apache.commons.math3.distribution.NormalDistribution;
 import org.apache.commons.math3.distribution.PoissonDistribution;
 
 public class Demand {
@@ -16,5 +17,21 @@ public class Demand {
          demandProbabilities [i] = dist.probability(i)/dist.cumulativeProbability(maxDemand);
       }
       return demandProbabilities;
+   }
+   
+   public static double[] normalProbability(int meanDemand, double stdPara, double tail) {
+	   
+	   double stdDemand = meanDemand * stdPara;
+	   
+	   NormalDistribution dist = new NormalDistribution(meanDemand, stdDemand);
+	   
+	   int maxDemand = (int) Math.floor(dist.inverseCumulativeProbability(1-tail));
+	   double[] demandProbabilities = new double[maxDemand + 1];
+	   
+	   for(int i=0; i<=maxDemand;i++) {
+		   demandProbabilities [i] = (dist.probability(i-0.5, i+0.5))/(dist.cumulativeProbability((double)maxDemand) - dist.cumulativeProbability(-0.5));
+	   }
+	   
+	   return demandProbabilities;
    }
 } 
