8de2de898ec387fcf41fc6cfb4eb3a8d445a4b24
xymmm <xyma26@163.com>
2019-10-30 13:32:33 +0000

    opl

--- src/main/java/PoissonPiecewise_sQ/solvePoissonPiecewise_sQ.java
+++ src/main/java/PoissonPiecewise_sQ/solvePoissonPiecewise_sQ.java
@@ -2,7 +2,14 @@ package PoissonPiecewise_sQ;
 
 import java.util.Timer;
 
+import ilog.cplex.IloCplex;
+import ilog.opl.IloOplDataSource;
+import ilog.opl.IloOplErrorHandler;
 import ilog.opl.IloOplFactory;
+import ilog.opl.IloOplModel;
+import ilog.opl.IloOplModelDefinition;
+import ilog.opl.IloOplModelSource;
+import ilog.opl.IloOplSettings;
 
 public class solvePoissonPiecewise_sQ {
 	
@@ -17,6 +24,16 @@ public class solvePoissonPiecewise_sQ {
 			//to create instances of OPL objects
 	        IloOplFactory.setDebugMode(true);
 	        IloOplFactory oplF = new IloOplFactory();
+	        IloOplErrorHandler errHandler = oplF.createOplErrorHandler();
+	        IloOplModelSource modelSource = oplF.createOplModelSource(DATADIR
+	                + "/sQ_1.mod");
+	        IloOplSettings settings = oplF.createOplSettings(errHandler);
+	        IloOplModelDefinition def = oplF.createOplModelDefinition(modelSource,settings);
+	        IloCplex cplex = oplF.createCplex();
+	        cplex.setOut(null);
+	        IloOplModel opl = oplF.createOplModel(def, cplex);
+	        IloOplDataSource dataSource = oplF.createOplDataSource(DATADIR
+	                + "/sQ_1.dat");
 		}catch() {
 			
 		}
