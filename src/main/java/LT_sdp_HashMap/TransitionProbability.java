d88be54c2bcdd3f8d9eb989e0a045aa2e85163ed
xymmm <xyma26@163.com>
2020-08-25 16:12:09 +0100

    prob

--- src/main/java/LT_sdp_HashMap/TransitionProbability.java
+++ src/main/java/LT_sdp_HashMap/TransitionProbability.java
@@ -5,4 +5,15 @@ public class TransitionProbability {
 	//TODO: to compute Poisson Distribution
 	//tail
 	//refer to probability distribution in sS
+	
+	//sdp.util.Demand.tabulateProbability(double meanDemand, double tail)
+	
+	public static double computeTransitProb(State state1, State state2, Action action, int meanDemand, double tail) {
+		double[] tabulateProbs = sdp.util.Demand.tabulateProbability(meanDemand, tail);
+		if((state1.i1 <= 0 )&&( state1.i2<=0)) {
+			return tabulateProbs[state1.i1 + action.quantityA - state2.i1-1]*tabulateProbs[state1.i2 + action.quantityB - state2.i2 -1];
+		}else {
+			return tabulateProbs[state1.i1 - action.transshipment + action.quantityA - state2.i1-1]*tabulateProbs[state1.i2 + action.transshipment + action.quantityB - state2.i2 -1];
+		}
+	}
 }
