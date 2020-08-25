cc6017abe594bb7cbb380c1cdf25167a1c74ff3f
xymmm <xyma26@163.com>
2020-08-25 21:26:01 +0100

    single period

--- src/main/java/LT_sdp_HashMap/ForwardRecursion.java
+++ src/main/java/LT_sdp_HashMap/ForwardRecursion.java
@@ -12,20 +12,24 @@ public class ForwardRecursion {
 		int[] demandMean = {2, 4, 6, 4};
 		StateSpace stateSpace = new StateSpace(-10,10);
 		int maxQuantity = 10;
-		ImmediateCost costPara = new ImmediateCost(10,0,5,0,1,2);
+		ImmediateCost costPara = new ImmediateCost(10,0,15,0,1,7);
 		HashMap<int[], Double> LT = new HashMap<int[], Double>();
 		
 		State initialState = new State(0,0);
 		double tail = 0.000001;
-		
-		
+			
 		int Stage = demandMean.length;
 		
-		double ETC = 0;
+		int[] optimalTransshipment = new int[Stage];
+		int[] optimalQuantityA = new int[Stage];
+		int[] optimalQuantityB = new int[Stage];
 		
+	
 		/******************** stage 1 *************************/
 		//generate states
 		List<int[]> allStates = StateSpace.generateStateSpace(stateSpace);
+		double[] ETC = new double [allStates.size()];
+		
 		//for(int i=0; i<allStates.size();i++) {System.out.println(Arrays.toString(allStates.get(i)));}		
 		for(int i=0; i<allStates.size(); i++) {
 			
@@ -40,21 +44,25 @@ public class ForwardRecursion {
 				Action action = new Action(keyList.get(c)[2], keyList.get(c)[3], keyList.get(c)[4]);
 				//if(i==100) {System.out.println("Action: "+ keyList.get(c)[2]+" "+keyList.get(c)[3]+" "+keyList.get(c)[4]);}
 				
-				double transitionProb = TransitionProbability.computeTransitProb(stateSpace, initialState, state, action, demandMean[0], tail);
-				if(i==100) {System.out.println(transitionProb);}
+				//double transitionProb = TransitionProbability.computeTransitProb(stateSpace, initialState, state, action, demandMean[0], tail);
+				//if(i==100) {System.out.println(transitionProb);}
 				
 				cost[c] = ImmediateCost.computeTransshipmentCost(action, costPara) 
 							+ ImmediateCost.computeReorderCost(action, costPara)
 							+ ImmediateCost.computeClosingCost(state, costPara);
 				int[] key = new int[] {state.i1, state.i2, action.transshipment, action.quantityA, action.quantityB};
 				LT.put(key, cost[c]);
-				ETC += transitionProb * cost[c];
 				//if(i==100) {System.out.println(Arrays.toString(key)+"\t"+cost[c]);}
 			}
 
 			
 		}
-		System.out.println(ETC);
+		/*
+		for(int i=0; i<ETC.length; i++) {
+			System.out.println(allStates.get(i)[0]+" " + allStates.get(i)[1]+"\t"+ETC[i]);
+		}
+		System.out.println(ETC.length);
+		*/
 		//for(int[] key: LT.keySet()){System.out.println(Arrays.toString(key) + "\t " + LT.get(key));} 
 
 	}
--- src/main/java/LT_sdp_HashMap/TransitionProbability.java
+++ src/main/java/LT_sdp_HashMap/TransitionProbability.java
@@ -5,30 +5,37 @@ import java.util.Arrays;
 import org.apache.commons.math3.distribution.PoissonDistribution;
 
 public class TransitionProbability {
-	
+
 	public static double computeTransitProb(StateSpace stateSpace, State state1, State state2, Action action, int demandMean, double tail) {
-		//double[] tabulateProbs = sdp.util.Demand.tabulateProbability(meanDemand, tail);
-		//System.out.println(Arrays.toString(tabulateProbs));
 		PoissonDistribution dist = new PoissonDistribution(demandMean);
-		if((state1.i1 <= 0 )&&( state1.i2<=0)) {
+		int maxDemand = dist.inverseCumulativeProbability(1-tail);
+
+		// compare with the max demand does not change the result, because infeasible actions, 
+		// including infeasible transshipment and order quantities, are already excluded from the set.
+		if((state1.i1 <= 0 )&&( state1.i2 <= 0)) {
 			if(
-					(state1.i1 + action.quantityA - state2.i1-1 >= stateSpace.minInventory)&&(state1.i1 + action.quantityA - state2.i1-1 <= stateSpace.maxInventory)&&
-					(state1.i2 + action.quantityB - state2.i2-1 >= stateSpace.minInventory)&&(state1.i2 + action.quantityB - state2.i2-1 <= stateSpace.maxInventory)
+					(state1.i1 + action.quantityA - state2.i1 <= maxDemand) && (state1.i1 + action.quantityA - state2.i1 >= 0)&&
+					(state1.i2 + action.quantityB - state2.i2 <= maxDemand) && (state1.i2 + action.quantityB - state2.i2 >= 0)
 					){
-				return dist.probability(state1.i1 + action.quantityA - state2.i1)*dist.probability(state1.i2 + action.quantityB - state2.i2);
+				return (dist.probability(state1.i1 + action.quantityA - state2.i1)/dist.cumulativeProbability(maxDemand))
+						*(dist.probability(state1.i2 + action.quantityB - state2.i2)/dist.cumulativeProbability(maxDemand));
 			}else {
 				return 0;
 			}			
+
 		}else {
 			if(
-					(state1.i1 - action.transshipment + action.quantityA - state2.i1 >= stateSpace.minInventory)&&(state1.i1 - action.transshipment + action.quantityA - state2.i1 <= stateSpace.maxInventory)&&
-					(state1.i2 + action.transshipment + action.quantityB - state2.i2 >= stateSpace.minInventory)&&(state1.i2 + action.transshipment + action.quantityB - state2.i2 <= stateSpace.maxInventory)
+					(state1.i1 - action.transshipment + action.quantityA - state2.i1 <= maxDemand)&&(state1.i1 - action.transshipment + action.quantityA - state2.i1 >= 0)&&
+					(state1.i2 + action.transshipment + action.quantityB - state2.i2 <= maxDemand)&&(state1.i2 + action.transshipment + action.quantityB - state2.i2 >= 0)
 					) {
-				return dist.probability(state1.i1 - action.transshipment + action.quantityA - state2.i1 )*dist.probability(state1.i2 + action.transshipment + action.quantityB - state2.i2);
+				return (dist.probability(state1.i1 - action.transshipment + action.quantityA - state2.i1 )/dist.cumulativeProbability(maxDemand))
+						*(dist.probability(state1.i2 + action.transshipment + action.quantityB - state2.i2)/dist.cumulativeProbability(maxDemand));
 			}else {
 				return 0;
 			}
-			
+
 		}
+
+
 	}
 }
